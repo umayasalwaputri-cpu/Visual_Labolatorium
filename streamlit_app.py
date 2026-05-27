4fe10b2e3d1c38d5ccbc45c20cf223e943b5d3e5
umayasalwaputri-cpu <umayasalwaputri@gmail.com>
2026-05-27 10:29:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import math
-from pathlib import Path
-
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='GDP dashboard',
